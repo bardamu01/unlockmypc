aa0ae261336920367bc5866224d8579e668813b5
bardamu01 <bardamu01@github.com>
2015-06-07 13:57:25 +0300

    minor refactoring

--- app/android/src/test/java/TestGetAWSConfig.java
+++ app/android/src/test/java/TestGetAWSConfig.java
@@ -6,14 +6,12 @@ import org.junit.Test;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class TestGetAWSConfig {
 
     @Test
     public void values_retrieved_ok() throws IOException {
-        InputStreamReader inputStreamReader = new FileReader("src/test/test.json");
-        AWSConfig awsConfig = AWSConfig.fromReader(inputStreamReader);
+        AWSConfig awsConfig = AWSConfig.fromReader(new FileReader("src/test/test.json"));
         Assert.assertEquals("endpoint", awsConfig.getEndpoint());
         Assert.assertEquals("access-key", awsConfig.getAccessKey());
         Assert.assertEquals("secret-key", awsConfig.getSecretKey());
